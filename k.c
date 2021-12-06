de0002456664d63338c92b99c3f0fcfe0a48406f
n <@>
2021-12-06 16:04:33 +0200

    k.c

--- k.c
+++ k.c
@@ -13,7 +13,7 @@ K Kc(Cv)_(ac(v))     K KC(C*a,Nn)_(aV(tC,n,a))                   C cK(Kx)_(xv)  
 K Ks(C*s)_(sym(s))   K KS(C**a,Nn)_(Ax=aS(n);i(n,xi=sym(a[i]))x)
 K Ki(Iv)_(az(v))     K KI(I*a,Nn)_(aV(tI,n,a))                   I iK(Kx)_(xv)  V IK(I*a,Kx){VK(tI,a,x);}
 K Kf(Dv)_(ad(v))     K KF(D*a,Nn)_(aV(tD,n,a))                   D fK(Kx)_(*xD) V FK(D*a,Kx){VK(tD,a,x);}
-                     K KL(K*a,Nn)_(aV(tA,n,a))                                  V LK(K*a,Kx){VK(tA,a,x);}
+                     K KL(K*a,Nn)_(a?aV(tA,n,a):aA(n))           V LK(K*a,Kx){VK(tA,a,x);}
 K Kp(V*p)_(al((L)p))
 
 K K1(Cv,Kx)_(v1[si(vc,v)](x))
