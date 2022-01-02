a52e48b43bce8fc1c8ac18cb0417f9286c200b6c
n <@>
2022-01-02 11:22:23 +0200

    p.c

--- p.c
+++ p.c
@@ -1,5 +1,4 @@
 #include"a.h" // ngn/k, (c) 2019-2021 ngn, GNU AGPLv3 - https://codeberg.org/ngn/k/raw/branch/master/LICENSE
-#define COM aw
 I si(Qs,Iv)_(SC0(s,v)-(C*)s)S D p10(In)_(Dv=1;i(n,v*=10)v)S Q pw(Qs)_(W(*s==32,s++)s)
 L pu(Q*p)_(Qs=*p;Lv=0;Cc=*s;W(c09(c),v=10*v+c-'0';c=*++s)*p=s;v)L pl(Q*p)_(Im=**p=='-';*p+=m;(1-2*m)*pu(p))
 S D pdu(Q*p)_(UL v=pu(p);Qs=*p;Cc=*s;P(c=='n'||c=='w',(*p)++;c=='n'?ND:WD)I e=0;
@@ -26,7 +25,7 @@ S A pt(C*v)_(Cc=*s;P(c=='`',s++;Ax=N(pS());xn>1?enl(x):x)P(c=='{',po())P(c=='"',
   W(1,p+=*p==32;B(!num(p))p+=*p=='-';c=*p;B(!cA9(c))W(cA9(c)||c=='.'||c==':',d|=si(".nwe",c)<4;c=*++p))p1(N(d?pD():pZ())))
  P(cAz(c),Qp=s;Ax=pP();I(s-p==1&&c-'y'<2u,k=max(k,c-'w'))AO(p-s0,p1(x)))P(c>>7,Qp=s;Ax=pP();*v=1;AO(p-s0,p1(x)))
  Ii=si("'/\\",c);P(i<3,c=*++s;I h=c==':';s+=h;*v=1;aw+i+3*h)i=si(vc,c);P(i>19,PLH)I u=*++s==':';s+=u;*v=1;AT(tv-u,i))
-S A1(pm,Xv(x^au^av)XA(I(xn==2&&xx==aw||xn==3&&xx==COM,x=mut(x);xA[xn-1]=pm(xA[xn-1]))x) //monadify
+S A1(pm,Xv(x^au^av)XA(I(xx==aw,x=mut(x);xA[xn-1]=pm(xA[xn-1]))x) //monadify
  Xs(Lv=xv;Qs=qs(&v);Nn;P(*s>>7&&s[(n=Sn(s))-1]-':',Cb[8];Mc(b,s,n);b[n]=':';b[n+1]=0;sym(b))x)x)
 S A pT(C*v)_(Ax=N(pt(v));
  W(1,Cc=*s;Ii=si("'/\\[",c);P(i>3,x)s++;
@@ -34,8 +33,8 @@ S A pT(C*v)_(Ax=N(pt(v));
   E(I u=*s==':';s+=u;x=a2(aw+i+3*u,x);*v=1))x)
 S V pW()_(s=pw(s);Cc=*s;P(c-'/')c=s[-1];P(s>s0&&c-32&&c-10)W((c=*++s)&&c-10))
 S A pe(Ax,C*v)_(pW();P(s>s0&&*s=='\\'&&s[-1]==32,s++;Ay=pe(0,v);P(!y,x?x(0):0);*v=0;y=a2(OUT,y);I(x,y=a2(pm(x),y))y)
- UH o=s-s0;Cb=0;Ay=pT(&b);P(!y,x?x(0):0)P(y==PLH,x?x:y)P(!b,Az=pe(y,v);P(!x,z)Nx(z);*v?a3(COM,x,z):AO(o,a2(pm(x),z)))
+ UH o=s-s0;Cb=0;Ay=pT(&b);P(!y,x?x(0):0)P(y==PLH,x?x:y)P(!b,Az=pe(y,v);P(!x,z)Nx(z);*v?a3(aw,x,z):AO(o,a2(pm(x),z)))
  Az=pe(0,v);P(!z,y(x?x(0):0))P(z==PLH,*v=1;P(!x,y)Yu(ep1(x))AO(o,a3(y,x,z)))
- *v&=y!=av;I(!x,y=pm(y))*v?a3(COM,x?AO(o,a3(y,x,PLH)):y,z):AO(o,x?a3(y,x,z):a2(pm(y),z)))
+ *v&=y!=av;I(!x,y=pm(y))*v?a3(aw,x?AO(o,a3(y,x,PLH)):y,z):AO(o,x?a3(y,x,z):a2(pm(y),z)))
 S A pb(Ax,Cc)_(W(1,Cv=0;Ay=Nx(pe(0,&v));I(y==PLH&&c-']',P(c==')',ep2(x,y))y=au)xq(y);B(*s-';'&&*s-10)s++)P(*s-c,ep1(x))s++;x)
 A pk(Qp)_(s0=s=p;Ax=pb(a1(PLH),0);P(!x,eQ(s0,Sn(s0),s-s0);0)x=shy(x);xn-2?x:las(x))
