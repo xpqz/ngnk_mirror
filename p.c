cc4e73b700c13280981278190f70e00108cd59d6
n <@>
2022-01-17 21:35:13 +0200

    shy()

--- p.c
+++ p.c
@@ -14,10 +14,9 @@ S A0(p0x,Ax=oC;s+=2;W(1,Cc=ph();P(c>15,x)s++;Cd=ph();P(d>15,ep1(x))s++;x=apc(x,c
 S A0(ps,Qp=s;Cc=*s;I(c>>7,W(*++s<-64)s+=*s==':')J(cAz(c),W(cA9(c),c=*++s))aCm(p,s))
 S A0(pS,Ax=oS;W(1,Ay=*s-'"'?ps():Nx(pC());y=str0(y);xq(y(sym(yV)));Qp=pw(s);P(*p-'`',x)s=p+1)0)
 S A0(pP,Ax=oS;W(1,Ay=str0(ps());y(xq(sym(yV)));P(*s-'.'||!cAz(s[1]),x)++s)0)
-S A1(shy,P(xn<2,x)Ay=xA[xn-1];I(ytA&&yn>2,Az=yx;I((z==av||ztu)&&_tsSA(yy),xq(au)))x)
 S A px(In)_(Q(n<4u);Ax=aS(n);i(xn,xi='x'+i)x)S A1(p1,xn-1?x:fir(x))S A pb(A,C);
 S A0(po,C k0=k;k=1;Q s1=s0,t=s0=s++;Ay;I(*s-'[',y=au)E(s++;y=sqz(N(pb(oA,']')));P(!ytS,ep1(y))P(yN>8,s--;ez1(y)))
- Az=pb(a1(PLH),'}');P(!z,s0=s1;y(0))I(y==au,y=px(k))Ax=N(cpl(aCn(t,s-t),shy(z),y));s0=s1;k=k0;x)
+ Az=pb(a1(PLH),'}');P(!z,s0=s1;y(0))I(y==au,y=px(k))Ax=N(cpl(aCn(t,s-t),z,y));s0=s1;k=k0;x)
 S A pt(C*v)_(Cc=*s;P(c=='`',s++;Ax=N(pS());xn>1?enl(x):x)P(c=='{',po())P(c=='"',p1(N(pC())))
  P(c=='(',s++;P(*s==')',s++;oA)Ax=N(pb(enl(MKL),')'));xn-2?x:las(x))P(c=='[',s++;pb(a1(PLH),']'))
  P(c09(c)&&s[1]==':',I u=s[2]==':';s+=2+u;Ii=20+c-'0';P(i>25,ep0())*v=1;AT(tv-u,i))P(c=='0'&&s[1]=='x',p1(N(p0x())))
@@ -37,4 +36,4 @@ S A pe(Ax,C*v)_(pW();P(s>s0&&*s=='\\'&&s[-1]==32,s++;Ay=pe(0,v);P(!y,x?x(0):0);*
  Az=pe(0,v);P(!z,y(x?x(0):0))P(z==PLH,*v=1;P(!x,y)Yu(ep1(x))AO(o,a3(y,x,z)))
  *v&=y!=av;I(!x,y=pm(y))*v?a3(aw,x?AO(o,a3(y,x,PLH)):y,z):AO(o,x?a3(y,x,z):a2(pm(y),z)))
 S A pb(Ax,Cc)_(W(1,Cv=0;Ay=Nx(pe(0,&v));I(y==PLH&&c-']',P(c==')',ep2(x,y))y=au)xq(y);B(*s-';'&&*s-10)s++)P(*s-c,ep1(x))s++;x)
-A pk(Qp)_(s0=s=p;Ax=pb(a1(PLH),0);P(!x,eQ(s0,Sn(s0),s-s0);0)x=shy(x);xn-2?x:las(x))
+A pk(Qp)_(s0=s=p;Ax=pb(a1(PLH),0);P(!x,eQ(s0,Sn(s0),s-s0);0)xn-2?x:las(x))
