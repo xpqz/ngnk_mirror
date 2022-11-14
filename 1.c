559215bfa586adac273e207ca0c6bb72a095063a
n <@>
2022-11-14 20:24:25 +0200

    1.c

--- 1.c
+++ 1.c
@@ -1,7 +1,15 @@
 #include<math.h> // ngn/k, (c) 2019-2022 ngn, GNU AGPLv3 - https://codeberg.org/ngn/k/raw/branch/master/LICENSE
 #include"a.h"
-A1(neg,XZF(Nn=xn;Lv=0;S4(xw,i(n,v+=xb==-128),i(n,v+=xh==-1<<15),i(n,v+=xi==-1<<31),)I(v,x=cT[xt+1](x))
-           Ay=xr>1?an(n,xt):x;SW(xt,C(tB,i(PD(n,xB),yb=-xb))C(tH,i(PD(n,xH),yh=-xh))C(tI,i(PD(n,xI),yi=-xi))C(tL,i(PD(n,xL),yl=-xl))D(i(PD(n,xF),yf=-xf)))x-y?x(y):y)
+A1(neg,
+ XZF(
+  Nn=xn;Lv=0;
+  S4(xw,
+   i(n,v+=xb==-1<< 7),
+   i(n,v+=xh==-1<<15),
+   i(n,v+=xi==-1<<31),
+  )
+  I(v,x=cT[xt+1](x))
+  Ay=xr>1?an(n,xt):x;SW(xt,C(tB,i(PD(n,xB),yb=-xb))C(tH,i(PD(n,xH),yh=-xh))C(tI,i(PD(n,xI),yi=-xi))C(tL,i(PD(n,xL),yl=-xl))D(i(PD(n,xF),yf=-xf)))x-y?x(y):y)
  Xzc(az(-gl(x)))Xf(af(-gf(x)))XC(neg(cB(x)))XmMA(e1f(neg,x))et1(x))
 A1(not,XmMA(e1f(not,x))XU(x(ai(x==au)))eql(xtsS?as(0):ai(0),x))
 A1(nul,XmMA(e1f(nul,x))XU(x(ai(x==au)))XF(Ay=aB(xn);xe(i(xn,yb=xf!=xf);y))Xf(x(ai(*xF!=*xF)))eql(cn[xt],x))
