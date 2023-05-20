a8c98b9c7c9465da6527784391e168f2d58897e7
n <@>
2023-05-20 18:52:11 +0300

    b.c, fix #55

--- b.c
+++ b.c
@@ -1,13 +1,14 @@
 #include"a.h" // ngn/k, (c) 2019-2023 ngn, GNU AGPLv3 - https://codeberg.org/ngn/k/raw/branch/master/LICENSE
-enum{bu,bv=32,bs=64,bg=80,bd=96,ba=112,bP,bi,bx,bI,bX,bm,bM,bG,bS,bl,bL,bz,bj,bo,bp,bc};            //opcodes
-S OCA di={                           1, 1, 2, 2, 1, 1, 2, 2, 1, 1, 1, 1, 1, 1         };            //extra bytes after opcode
-S OCA ds={                           1, 1,-2,-1,-3,-2,-1,-1, 1,-1, 1, 0,-1, 0, 1,-1   };            //stack size delta
+enum{bu,bv=32,bs=64,bg=80,bd=96,ba=112,bP,bx,bX,bm,bM,bG,bS,bl,bL,bz,bj,bo,bp,bc};                  //opcodes
+S OCA di={                           1, 1, 2, 1, 2, 2, 1, 1, 1, 1, 1, 1         };                  //extra bytes after opcode
+S OCA ds={                           1, 1,-1,-2,-1,-1, 1,-1, 1, 0,-1, 0, 1,-1   };                  //stack size delta
 #define h(a) {b[nb]=a;m[nb]=o;nb+=nb<255;}                                                          //append byte
 #define Nc(a) N(cc(a,o));                                                                           //append "load constant" instruction; return on error
 #define Nr(a...) {I r_=cr(a);P(r_-OK,r_);}                                                          //compile rvalue; return on error
 #define Nl(a...) {I r_=cl(a);P(r_-OK,r_);}                                                          //compile lvalue; return on error
-#define OK -1
-S A u,cr(A,I);S UC*b,*m;S I nb,nl,l[16],lu[16];                                                     //b,nb:bytecode, m:sourcemap, l,nl:locals symbols, lu:last usages of locals
+#define OK -1                                                                                       //returned by cl() and cr() on success
+S A u,cr(A,I);                                                                                      //u:lambda object (src;bytecode;map;locals;constants..)
+S UC*b,*m;S I nb,nl,l[16],lu[16];                                                                   //b,nb:bytecode, m:sourcemap, l,nl:locals symbols, lu:last usages of locals
 SN I il(Iv)_(Li=fI(l,nl,v);P(i<0,-1)lu[i]=nb;i)                                                     //index of a local  variable
 SN I ig(Lv)_(                                                                                       //index of a global variable
  I(*gp,Qs=qs(&v);
@@ -15,7 +16,7 @@ SN I ig(Lv)_(                                                                   
    Nm=SL(gp),n=SL(s);P(m+n+3>SZ gp,-1)gp[m]='.';MC(gp+m+1,s,n+1);v=(I)sym(gp);gp[m]=0))
  Li=fI(gk,gn,v);P(i>=0,i)P(gn-(UC)gn,-1)gk[gn]=v;gv[gn]=0;gn++)
 S I cc(Ax/*1*/,I o)_(                                                                               //append "load constant" instruction
- Ay=u;Nn=yn,i=5;W(i<n,B(mtc_(x,ya),x=x(0))i++)I(x,P(bc+i-5>255,ez1(x))uq(x))h(bc+i-5)1)
+ Ay=u;Nn=yn,i=4;W(i<n,B(mtc_(x,ya),x=x(0))i++)I(x,P(bc+i-4>255,ez1(x))uq(x))h(bc+i-4)1)
 S A cl(Ax,Ay/*00*/,I r)_(Q(xx==av||_t(xx)==tu)Iv=_v(xx),o=xo;                                       //compile lvalue
  Y(RsS(I(yts,P(xx==av&&nl,Li=il(yv);I(i<0,i=nl;P(i>15,ez0())Iv=yv;l[nl++]=v;lu[i]=nb)
                               h(bs+i)I(r,h(bg+i))OK)
@@ -24,8 +25,8 @@ S A cl(Ax,Ay/*00*/,I r)_(Q(xx==av||_t(xx)==tu)Iv=_v(xx),o=xo;                   
        Ii=ig(yv);P(i<0,ez0())h(v?bM:bS)h(i)I(v,h(v))I(r,h(bG)h(i))OK)
    RA(In=yn-1;P(n-(UC)n||n<1,o)Az=yx;
       P(z==MKL&&(xx==av||_t(xx)==tu),h(bL)h(n)i(n,Nl(x,yA[i+1],0))I(!r,h(bp))OK)
-      ZsS(I(ztS,z=jS(zR))i(n,Nr(yA[n-i],1))h(bl)h(n)Ii=il(zv);P(i<0,Nc(z)h(r?bX:bI)h(v)OK)
-          h(r?bx:bi)h(i)h(v)OK)o))o)
+      ZsS(I(ztS,z=jS(zR))i(n,Nr(yA[n-i],1))h(bl)h(n)Ii=il(zv);P(i<0,Nc(z)h(bX)h(v)I(!r,h(bp))OK)
+          h(bx)h(i)h(v)I(!r,h(bp))OK)o))o)
 S B cv(Lv)_(Qs=qs(&v);Nn=SL(s);n&&s[n-1]==':')                                                      //does symbol v end with a ":"?
 S A cr(Ax/*0*/,I r)_(I o=xo;                                                                        //compile rvalue
  XsS(I(xts,Ii=il(xv);P(i>=0,h(bg+i)I(!r,h(bp))OK)P(xv=='o',I(r,h(bo))OK))
@@ -65,40 +66,36 @@ S I mxs(Ii,I s)_(I r=s;                                                         
         ((c==bL)-(c==bl||c==ba||c==bP))*b[i];
      i+=(c-ba<L(di)?di[c-ba]:0)+(c==bj)*b[i];
      I(c==bz,r=MAX(r,mxs(i+b[i-1],s))))r)
-S I shy(Ax/*0*/)_(                                                                                  //set result to :: if last statement in ast is assignment
+S I shy(Ax/*0*/)_(                                                                                  //is the last expression in the tree x an assignment?
  !xtA?0:xn&&xx==PLH?shy(xA[xn-1]):xn==3&&(xx==av||_t(xx)==tu||(_t(xx)==ts&&cv(_v(xx))))&&_tsSA(xy))
-A3(cpl,/*src,ast,loc 111*/u=aV(tA,5,A(x,au,au,au,au));                                              //compile
- UC b0[256],m0[256];b=b0;m=m0;nb=1;I(z,nl=zn;MC(l,zV,4*nl);z(0))E(nl=0)Ik=nl;
- MS(lu,-1,SZ lu);y=Nu(cf(y));I s=shy(y),r=cr(y,1);y(0);I o=0;I(s,Nc(au))h(bu)
- P(r-OK,ec0();eS(ux,r);u(0))P(nb>=255||bc-4+un>255,eS(ux,0);u(0);ez0())
- i(16,Ij=lu[i];I(j>=0&&b[j]==bg,b[j]=bd))*b=mxs(1,0);*m=-1;uy=aCn(b,nb);uz=aCn(m,nb);
- uA[3]=aV(tS,nl,l);AK(k,AT(to,u)))
+A3(cpl,/*src,ast,loc 111*/UC b0[256],m0[256];b=b0;m=m0;nb=1;MS(lu,-1,SZ lu);                        //compile
+ I(z,nl=zn;MC(l,zV,4*nl);z(0))E(nl=0)Ik=nl;u=aA(4);ux=x;uy=uz=uA[3]=au;
+ y=Nu(cf(y));I s=shy(y),r=cr(y,1);y(0);P(r-OK,ec0();eS(ux,r);u(0))I o=0;I(s,Nu(cc(au,o)))h(bu)
+ P(nb>=255||un>255-bc+4,eS(ux,0);u(0);ez0())i(nl,Ij=lu[i];I(j>=0&&b[j]==bg,b[j]=bd))
+ *b=mxs(1,0);*m=-1;uy=aCn(b,nb);uz=aCn(m,nb);uA[3]=aV(tS,nl,l);AK(k,AT(to,u)))
 
 #define U(x,a...) I(!(x),a;goto l)
 AX(run,Q(xto)P(n-xk,er8(a,n))S I d;P(++d>2048,es8(a,n))UC*b=_V(xy),c,ns=*b++,nl=_n(xA[3]);          //virtual machine
  A t[ns+nl],*s=t+ns,*l=s;MS(t,0,SZ t);MC(l,a,8*n);                                                  //
- W((c=*b++),                                                                                        //   MEANING    |BYTES |         STACK           |         EFFECT
-  I(c>=bc,*--s=_R(xA[c-bc+5]))                                                                      //load constant |bc+n  |.. -> .. consts[n]       |
-  J(c>=ba,SW(c,                                                                                     //              |      |                         |
-   C2(ba,bP,Nn=*b++;Ax=*s,*p=s+1;s+=n;U(*s=x((c==ba?_8:prj)(x,p,n))))                               //apply|project |ba,n  |.. z y x f -> .. f[x;y;z]|
-   C(bi,Nn=*b++;Ax=l[n],y=*s++;I(!x,x=au)x=l[n]=y(d4(x,y,av+*b++,*s));mr(*s);U(x,*s=0)s++)          //              |bi,n,v|                         |
-   C(bx,Nn=*b++;Ax=l[n],y=*s++;I(!x,x=au)x=l[n]=  d4(x,y,av+*b++,*s) ;mr(*s);U(x,*s=y(0))U(*s=dot(x,y)))//          |      |                         |
-   C(bI,Nn=*b++;Ax=*s++,y=*s++,z=*s;U(x=y(z(d8(A(x,y,av+n,z),4))),*s=0)s++)                         //              |      |                         |
-   C(bX,Nn=*b++;Ax=*s++,y=*s++,z=*s;U(x=  z(d8(A(x,y,av+n,z),4)),*s=y(0))U(*s=x(dot(x,y))))         //              |      |                         |
-   C2(bm,bM,Nn=*b++;A*p=(c==bm?l:gv)+n,x=*p;U(x,*s=ev1(*s))Ay=v2[*b++](x,*s++);U(y,*--s=0)*p=x(y))  //modified asgn |bm,n,v|.. x -> ..               |vars[n]:dyads[v][vars[n];x]
-   C(bG,Nn=*b++;Ax=*--s=gv[n];U(x,ev0())xR)                                                         //get global    |bG,n  |.. -> .. globals[n]      |
-   C(bS,Nn=*b++;Ax=*s++,y=gv[n];gv[n]=y?y(x):x)                                                     //set global    |bS,n  |.. x -> ..               |globals[n]:x
-   C(bl,Nn=*b++;s+=n-1;*s=sqz(aV(tA,n,s-n+1)))                                                      //create list   |bl,n  |.. z y x -> .. (x;y;z)   |
-   C(bL,Nn=*b++;Ax=*s;U(xtt||xN==n,*s=el1(x))i(n,*--s=ii(x,n-1-i)))                                 //destruct list |bL,n  |.. x -> .. x[0] x[1] x[2]|
-   C(bj,b+=*b+1)                                                                                    //jump          |bj,n  |.. x -> ..               |PC:n
-   C(bz,Nn=*b++;b+=n*!tru(*s++))                                                                    //branch        |bz,n  |.. x -> ..               |if x is falsy, PC+:n
-   C(bo,*--s=xR)                                                                                    //recur         |bo    |.. -> .. o               |o is the current lambda
-   C(bp,mr(*s++))                                                                                   //pop           |bp    |.. x -> ..               |
-   D(Q(0))))                                                                                        //              |      |                         |
-  J(c>=bs,A*v=l+c%16,x=*v;                                                                          //              |      |                         |
-   I(c>=bd,*--s=x;*v=0)                                                                             //delete local  |bd+n  |.. -> ..                 |locals[n]:NULL (freed)
-   J(c>=bg,U(*--s=x)xR)                                                                             //get local     |bg+n  |.. -> .. locals[n]       |
-   E(Q(c>=bs)Ay=*s++;*v=x?x(y):y))                                                                  //set local     |bs+n  |.. x -> ..               |locals[n]:x
-  J(c>=bv,Ax=*s++;U(*s=x(v2[c-bv](x,*s))))                                                          //dyadic  verb  |bv+n  |.. y x -> dyads[n][x;y]  |
-  E(U(*s=v1[c](*s))))                                                                               //monadic verb  |bu+n  |.. x -> monads[n][x]     |
- l:I(!*s,eS(xx,(UC)_C(xz)[(C*)b-1-_C(xy)]))i(t+ns+nl-s-1,Ax=s[i+1];I(x,mr(x)))d--;/*Q(s==l-1)*/*s)
+ W((c=*b++),                                                                                        //             |BYTES |          STACK          |         EFFECT
+  I(c>=bc,*--s=_R(xA[c-bc+4]))                                                                      //load constant|bc+i  |.. -> .. consts[i]       |
+  J(c>=ba,SW(c,D(Q(0))                                                                              //             |      |                         |
+   C2(ba,bP,Nn=*b++;Ax=*s,*p=s+1;s+=n;U(*s=x((c==ba?_8:prj)(x,p,n))))                               //apply|project|ba,n  |.. z y x f -> .. f[x;y;z]|
+   C(bx,A*p=l+*b++,x=*p,y=*s++;x=*p=d4(x?x:au,y,av+*b++,*s);mr(*s);U(x,*s=y(0))U(*s=dot(x,y)))      //             |bx,l,d|.. x -> .. r             |r:locals[l]:dyads[d][locals[l];x]
+   C(bX,Ax=*s++,y=*s++,z=*s;U(x=z(d8(A(x,y,av+*b++,z),4)),*s=y(0))U(*s=x(dot(x,y))))                //             |bX,d  |.. x -> .. r             |r:.[x;y;dyads[d];z]
+   C2(bm,bM,A*p=(c==bm?l:gv)+*b++,x=*p;U(x,*s=ev1(*s))Ay=v2[*b++](x,*s++);U(y,*--s=0)*p=x(y))       //modified asgn|bm,l,d|.. x -> ..               |locals[l]:dyads[d][locals[l];x]
+   C(bG,Ax=*--s=gv[*b++];U(x,ev0())xR)                                                              //get global   |bG,g  |.. -> .. globals[g]      |
+   C(bS,A*p=gv+*b++,x=*s++,y=*p;*p=y?y(x):x)                                                        //set global   |bS,g  |.. x -> ..               |globals[g]:x
+   C(bl,Nn=*b++;s+=n-1;*s=sqz(aV(tA,n,s-n+1)))                                                      //make list    |bl,n  |.. z y x -> .. (x;y;z)   |
+   C(bL,Nn=*b++;Ax=*s;U(xtt||xN==n,*s=el1(x))i(n,*--s=ii(x,n-1-i)))                                 //break list   |bL,n  |.. x -> .. x[0] x[1] x[2]|
+   C(bj,b+=*b++)                                                                                    //jump         |bj,a  |.. x -> ..               |PC+:a
+   C(bz,b+=*b++*!tru(*s++))                                                                         //branch       |bz,a  |.. x -> ..               |if x is falsy, PC+:a
+   C(bo,*--s=xR)                                                                                    //recur        |bo    |.. -> .. o               |o is the current lambda
+   C(bp,mr(*s++))))                                                                                 //pop          |bp    |.. x -> ..               |
+  J(c>=bs,A*v=l+c%16,x=*v;                                                                          //             |      |                         |
+   I(c>=bd,*--s=x;*v=0)                                                                             //delete local |bd+l  |.. -> .. locals[l]       |locals[l]:NULL (freed)
+   J(c>=bg,U(*--s=x)xR)                                                                             //get local    |bg+l  |.. -> .. locals[l]       |
+   E(Q(c>=bs)Ay=*s++;*v=x?x(y):y))                                                                  //set local    |bs+l  |.. x -> ..               |locals[l]:x
+  J(c>=bv,Ax=*s++;U(*s=x(v2[c-bv](x,*s))))                                                          //dyadic  verb |bv+d  |.. y x -> dyads[d][x;y]  |
+  E(U(*s=v1[c](*s))))                                                                               //monadic verb |bu+m  |.. x -> monads[m][x]     |
+ l:I(!*s,eS(xx,(UC)_C(xz)[(C*)b-1-_C(xy)]))i(t+ns+nl-s-1,Ax=s[i+1];I(x,mr(x)))d--;*s)
