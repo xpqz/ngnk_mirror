1e58f989be240bbf3d2dbcba86873d6e8245fae0
n <@>
2023-05-28 22:13:23 +0300

    b.c

--- b.c
+++ b.c
@@ -44,50 +44,39 @@ SN I il(Iv)_(Li=fI(l,nl,v);P(i<0,-1)lu[i]=nb;i)                                 
 SN UC ig(Lv)_(Qs=qs(&v);                                                                            //index of a global variable (allocates a new slot if not found)
  I(*gp&&!cd(v)&&id0(*s),Nm=SL(gp),n=SL(s);P(m+n+3>SZ gp,-1)gp[m]='.';MC(gp+m+1,s,n+1);v=(I)sym(gp);gp[m]=0)//prepend \d prefix
  Li=fI(gk,gn,v);P(i>=0,i)P(gn>=L(gv),0)gk[gn]=v;gv[gn]=0;gn++)
-S B cm(Ax/*0*/){X(Rv(!xv)Ru(1)Rs(Lv=xv;Qs=qs(&v);Nn=SL(s);n&&s[n-1]==':')R_(0))}                    //is x a valid modifier? i.e. :: or primitive monad or symbol ending with ":"
+SN UC igS(Ax)_(Q(xtS)x=jS(xR);Lv=xv;Qs=qs(&v);                                                      //index of a global variable (allocates a new slot if not found)
+ I(*gp&&!cd(v)&&id0(*s),Nm=SL(gp),n=SL(s);P(m+n+3>SZ gp,-1)gp[m]='.';MC(gp+m+1,s,n+1);v=(I)sym(gp);gp[m]=0)//prepend \d prefix
+ Li=fI(gk,gn,v);P(i>=0,i)P(gn>=L(gv),0)gk[gn]=v;gv[gn]=0;gn++)
+S B cm(Ax/*0*/){X(Rv(!xv)Ru(1)RS(P(xn-1,0)Lv=*xI;Qs=qs(&v);Nn=SL(s);n&&s[n-1]==':')R_(0))}          //is x a valid modifier? i.e. :: or primitive monad or symbol ending with ":"
 S V cc(Ax/*1*/,I o){Nn=un,i=CO;W(i<n&&!mtc_(x,ua),i++)i<n?x(0):uq(x);h(i+bc-CO);}                   //append a "load constant" instruction
 S I cl(Ax,Ay/*00*/,B r){Q(cm(xx))Iv=_v(xx),o=xo;                                                    //compile lvalue (x:assignmentNode,y:tree,r:wantResult)
  Y(R_(o)
-   Rs(I w=yv;I(!cd(w),Ii=il(w);P(xx==av&&nl,I(i<0,i=nl;P(i>15,o)l[nl++]=w;lu[i]=nb)h(bs+i)I(r,h(bg+i))OK)P(i>=0,h(bm)h(i)h(v)I(r,h(bg+i))OK))
-      UC i=ig(w);h(v?bM:bS)h(i)I(v,h(v))I(r,h(bG)h(i))OK)
-   RS(die("NYI"))
+   RS(I(yn==1,I w=*yI,i=il(w);P(xx==av&&nl,I(i<0,i=nl;P(i>15,o)l[nl++]=w;lu[i]=nb)h(bs+i)I(r,h(bg+i))OK)P(i>=0,h(bm)h(i)h(v)I(r,h(bg+i))OK))
+      UC i=igS(y);h(v?bM:bS)h(i)I(v,h(v))I(r,h(bG)h(i))OK)
    RA(In=yn-1;P(!n||n>8u,o)Az=yx;P(z==MKL&&(xx==av||_t(xx)==tu),h(bL)h(n)i(n,Nl(x,yA[i+1],0))I(r,P(xx-av,o))E(h(bP))OK)
-      Zs(i(n,Nr(yA[n-i],1))h(bl)h(n)Ii=cd(zv)?-1:il(zv);I(i>=0,h(r?by:bx))E(i=ig(zv);h(r?bY:bX))h(i)h(v)OK)o))}
+      ZS(i(n,Nr(yA[n-i],1))h(bl)h(n)Ii=zn-1?-1:il(*zI);I(i>=0,h(r?by:bx))E(i=igS(z);h(r?bY:bX))h(i)h(v)OK)o))}
 S I cr(Ax/*0*/,B r)_(I o=xo;                                                                        //compile rvalue (x:tree,r:wantResult)
- Xs(Iv=xv,i=cd(v)?-1:il(v);I(i>=0,h(bg+i))J(v-'o',h(bG)h(ig(v)))E(h(bo))I(!r,h(bP))OK)              // x.y      variable (possibly qualified)
- XS(die("NYI"))
+ XS(Ii=xn-1?-1:il(*xI);I(i>=0,h(bg+i))J(xn==1&&*xI=='o',h(bo))E(h(bG)(h(igS(x))))I(!r,h(bP))OK)     // x.y      variable (possibly qualified)
  P(!xtA||!xn,I(r,cc(x-GAP?xR:au,o))OK)                                                              // 0        constant
  Nn=xn;Ay=xx;                                                                                       //
  P(y==GAP,i(n-1,Nr(xA[i+1],i==n-2&&r))OK)                                                           // [x;y]    block
  P(n==1,I(r,cc(yR,o))OK)                                                                            // `a       quoted
- P(n==3&&cm(y)&&_tsSA(xy),P(!yts,Nr(xz,1);Nl(x,xy,r);OK)                                            // x+:y     modified assignment
-  Nr(xz,1);Nr(xy,1);Nr(cS(drp(-1,str(y))),1);h(ba)h(2)Az=aA1(au);Nl(z,xy,r);z(0);OK)                // x⊕:y     modified assignment (unicode verbs)
+ P(n==3&&cm(y)&&_tsSA(xy),YS(Nr(xz,1);Nr(xy,1);Nr(enl(cS(drp(-1,str(ii(y,0))))),1);h(ba)h(2)Az=aA1(au);Nl(z,xy,r);z(0);OK)Nr(xz,1);Nl(x,xy,r);OK)// x[y]+:z     assignment
  P(n>3&&(y==av||y==CST),n--;I p[n];A*a=xA;i(n&~1,Nr(*++a,1);h(i&1?bj:bz)p[i]=nb;h(0))               // :[x;y;z] cond
   Nr(n&1?*++a:au,1);i(n&~1,I d=(i&1?nb-1:p[i+1])-p[i];I(i&1,Ij=(n&~1)-1;W(i<j&&d>255,d=p[j]-1-p[i];j-=2))P(d>255,o)b[p[i]]=d)I(!r,h(bP))OK)
  I(n==2&&y==FIR,Az=xy;I(ztA&&zn==2,P(zx-REV<3u,Nr(zy,1);h(bu+zx-REV+LAS-au)I(!r,h(bP))OK)))         // *|x      recognized idioms
  I p=0;i(n-1,Az=xA[n-1-i];I(z-GAP,Nr(z,1))E(p=1;cc(GAP,o)))I(p,Nr(xx,1);h(bp)h(n-1))                // x[y;]    projection
  J(y==MKL,n--;P(n>255u,o)h(bl)h(n))                                                                 // (x;y)    list
  J(n==2&&ytu,h(bu+yv))                                                                              // +x       monad
- J(n==3&&ytv,I(!p&&!_tsSA(xy),Q(b[nb-1]>=bc);Ii=b[nb-1]-bc;b[nb-1]=bV;h(i)h(yv))E(h(bv+yv)))        // x+y      dyad
+ J(n==3&&ytv,I(!p&&!_tSA(xy),Q(b[nb-1]>=bc);Ii=b[nb-1]-bc;b[nb-1]=bV;h(i)h(yv))E(h(bv+yv)))         // x+y      dyad
  E(P(n>9,o)Nr(xx,1)h(ba)h(n-1))                                                                     // x[y]     application
  I(!r,h(bP))OK)
-A1(qte,/*1*/xtsSA?aA1(x):x)                                                                         //quote
-S A2(c2,/*00*/P(xtw&&!ytsSA,1)/*P(x==TIL&&ytZ&&yn<4,i(yn,P(!IN(gl(ii(y,i)),101),0))1)*/0)           //constant folding
+A1(qte,/*1*/xtS||xtA?aA1(x):x)                                                                      //quote
+S A2(c2,/*00*/P(xtw&&!ytSA,1)/*P(x==TIL&&ytZ&&yn<4,i(yn,P(!IN(gl(ii(y,i)),101),0))1)*/0)            //constant folding
 S A3(c3,/*000*/P(ADD<=x&&x<=MUL&&ytzZ&&ztzZ&&(ytt||ztt||yn==zn)&&MAX(xN,yN)<101,1)0)                //constant folding
-S A1(cf,P(!xtA||!xn,x)P(xx==MKL,i(xn,Ay=xa;YsSA(x))qte(N(drp(1,x))))P(xn==2?c2(xx,xy):xn==3?c3(xx,xy,xz):0,qte(N(val(x))))Ay=rsz(xn,au);i(xn,ya=cf(xa);xa=au;P(!ya,die("CF")))AO(xo,x(y)))
+S A1(cf,P(!xtA||!xn,x)P(xx==MKL,i(xn,Ay=xa;YSA(x))qte(N(drp(1,x))))P(xn==2?c2(xx,xy):xn==3?c3(xx,xy,xz):0,qte(N(val(x))))Ay=rsz(xn,au);i(xn,ya=cf(xa);xa=au;P(!ya,die("CF")))AO(xo,x(y)))
 S I mxs(Ii,I s)_(I r=s;W(1,UC c=MIN(bc,b[i++]);r=MAX(r,s);P(!c,r)s+=ds[c]+ks[c]*b[i];i+=di[c]+(c==bj)*b[i];I(c==bz,r=MAX(r,mxs(i+b[i-1],s))))r)//max stack
-S B shy(Ax/*0*/)_(!xtA?0:xn&&xx==GAP?shy(xA[xn-1]):xn==3&&cm(xx)&&_tsSA(xy))                        //is last expr an assignment?
+S B shy(Ax/*0*/)_(!xtA?0:xn&&xx==GAP?shy(xA[xn-1]):xn==3&&cm(xx)&&_tSA(xy))                        //is last expr an assignment?
 A3(cpl,/*111*/nb=1;MS(lu,-1,SZ lu);Ik=0;I(z,k=zn;MC(l,zV,CO*k);z(0))nl=k;u=aA(CO);y=Nx(cf(y));ux=x;uy=uz=uA[3]=au;B s=shy(y);I r=cr(y,!s);y(0);P(r-OK,ec0();eS(ux,r);u(0))
  I o=0;I(s,cc(au,o))P(un>255||nb>L(b)-2||nl>L(l)-2||gn>L(gv)-2,ez0();eS(ux,0);u(0))h(bu)P(nb>=255||un>255-bc+CO,eS(ux,0);u(0);ez0())
  i(nl,Ij=lu[i];I(j>=0&&b[j]==bg,b[j]=bd))*b=mxs(1,0);*m=-1;uy=aCn(b,nb);uz=aCn(m,nb);uA[3]=aV(tS,nl,l);AK(k,AT(to,u)))
-
-//┌────┬───────────┐
-//│CODE│    AST    │
-//│    │ old   new │
-//├────┼─────┬─────┤
-//│a   │`a   │,`a  │
-//│a.b │`a`b │`a`b │
-//│`a  │,`a  │`a   │
-//│,`a │,,`a │,,`a │
-//│`a`b│,`a`b│,`a`b│
-//└────┴─────┴─────┘
