6c656a496b2a3bbde044caca099ec63c4620fb86
n <@>
2024-01-04 00:07:29 +0200

    m.c

--- m.c
+++ m.c
@@ -5,18 +5,18 @@
 #ifndef MAP_NORESERVE
  #define MAP_NORESERVE 0
 #endif
-Z ST{V*p;U n;B f;}reg[128];Z U nreg;Z UC pnd[128];Z U npnd;
-Z V mc(){P(!npnd)F(npnd,I j=pnd[i];munmap(reg[j].p,reg[j].n);Q(reg[j].f);reg[j].p=0)npnd=0;I j=0;F(nreg,I(reg[i].p,MC(reg+j,reg+i,SZ*reg);j++))nreg=j;}
+Z ST{V*p;W n;B f;}reg[128];Z U nreg;Z UC pnd[128];Z U npnd;
+Z V mc(){P(!npnd)F(npnd,U j=pnd[i];Q(reg[j].f);munmap(reg[j].p,reg[j].n);reg[j].p=0)npnd=0;U j=0;F(nreg,I(reg[i].p,MC(reg+j,reg+i,SZ*reg);j++))nreg=j;}
 Z A mu(V*p)_(F(nreg,P(reg[i].p==p,pnd[npnd++]=i;0))die("UNMAP"))
-Z V*mm(U n,U f)_(V*p=mmap(0,n,PROT_READ|PROT_WRITE,MAP_NORESERVE|MAP_PRIVATE|MAP_ANON,-1,0);P((L)p<pg,(V*)0)I(nreg==L(reg),mc();I(nreg==L(reg),die("MMAP")))reg[nreg++]=(TY(*reg)){p,n,f};p)
+Z V*mm(W n,U f)_(V*p=mmap(0,n,PROT_READ|PROT_WRITE,MAP_NORESERVE|MAP_PRIVATE|MAP_ANON,-1,0);P((L)p<pg,(V*)0)I(nreg==L(reg),mc();I(nreg==L(reg),die("MMAP")))reg[nreg++]=(TY(*reg)){p,n,f};p)
 A mf(U f,U i,U n)_(V*p=mm(pg+n,1);P(!p,eo0())A x=(A)p+pg;xn=n;AT(tC,x);P(mmap(p+pg,n,PROT_READ|PROT_WRITE,MAP_NORESERVE|MAP_PRIVATE|MAP_FIXED,f,i)!=p+pg,mu(p);eo0())xR)
 
-Z A b[24];DBG(Z U lck;)
+Z A bkt[24];DBG(Z U lck;)
 Z W cap(A x/*0*/)_((HD<<xb)-HD)//chunk capacity in bytes
-A1(m0,DBG(lck++;)Q(x)XP(0)Q(xr>0)P(--xr,0)U i=xb;P(!i,mu(xV-pg))xX=b[i];b[i]=x;XR(mrn(xn|!xn,xA);x)x)
+A1(m0,DBG(lck++;)Q(x)XP(0)Q(xr>0)P(--xr,0)U i=xb;P(!i,mu(xV-pg))xX=bkt[i];bkt[i]=x;XR(mrn(xn|!xn,xA);x)x)
 DBG(A1(m1,lck--;P(!x||!xb,0)MS(xV,0xab,cap(x));AN(-1,AT(0,x));0))
 A1(_R,Q(x);XP(x)Q(xr>=0);xr++;x)A1(mr,DBG(m1)(m0(x)))V mRn(U n,CO A*a){F(n,_R(a[i]))}V mrn(U n,CO A*a){F(n,mr(a[i]))}A1(mRa,mRn(xn,xA);x)
-ZN A mb(U i)_(P(i>=L(b),V*p=mm(HD<<i,0);P(!p,die("OOM"))A x=(A)p+HD;xb=i;x)A x=b[i];P(x,b[i]=xX;xX=0;x)x=mb(i+1);xb=i;A y=x+(HD<<i);yb=i;AT(0,y);yr=0;yX=b[i];b[i]=y;x)
+ZN A mb(U i)_(P(i>=L(bkt),V*p=mm(HD<<i,0);P(!p,die("OOM"))A x=(A)p+HD;xb=i;x)A x=bkt[i];P(x,bkt[i]=xX;xX=0;x)x=mb(i+1);xb=i;A y=x+(HD<<i);yb=i;AT(0,y);yr=0;yX=bkt[i];bkt[i]=y;x)
 
 A an(U n,C t)_(Q(!lck)Q(tA<=t)Q(t<tn)Q(!TP(t))W m=HD+(((W)n<<Tw[t])+7>>3);U i=59-CLZ(HD|m-1);A x=mb(i);xr=1;AT(t,AN(n,x)))
 A aV(C t,U n,CO V*v)_(A x=an(n,t);MC(xV,v,((W)n<<Tw[t])+7>>3);x)
